f1f646bb8efec0085a9ddd53b3a349edf922deda
Jack Frings <fringsj26@student.jhs.net>
2024-03-16 09:13:16 -0700

    cmake package

---
+++ scripts/img_capture.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+import rclpy
+import cv2
+from core_lib.img_capture_module import ImageCaptureNode
+import os
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageCaptureNode()
+
+    # Delete prior images
+    try: os.system("rm {}/*.png".format(node.path))
+    except: pass
+    try: os.system("rm {}/coral/*.png".format(node.path))
+    except:pass
+    
+    rclpy.spin(node)
+
+    cv2.destroyAllWindows()
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
